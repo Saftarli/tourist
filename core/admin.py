44fb4ad356a48e5d9eb5b4d95110869c97bd3a2e
saftarli <tofiqsefterli@gmail.com>
2025-05-09 08:06:03 +0400

    Site Settings,Tour Detal, Contact Information

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from core.models import SiteSettings
 # Register your models here.
+
+class SingleInstanceAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        if self.model.objects.exists():
+            return False
+        return True
+
+
+class SiteSettingsAdmin(SingleInstanceAdmin):
+    pass
+admin.site.register(SiteSettings,SingleInstanceAdmin)
